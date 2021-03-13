690383266d11691889831102eaf6b2b1d3ee5069
Yuki Kitayama <yk2797@columbia.edu>
2021-03-13 15:04:56 -0700

    finally finished a3c cartpole

--- algorithm/a3c_cartpole.py
+++ algorithm/a3c_cartpole.py
@@ -9,10 +9,9 @@ import gym
 import os
 import pickle
 
-# I don't know what it is, but without it, it works
-# os.environ['OMP_NUM_THREADS'] = "1"
-
 # Parameter
+# https://stackoverflow.com/questions/43894608/use-of-omp-num-threads-1-for-python-multiprocessing
+os.environ['OMP_NUM_THREADS'] = "1"
 # device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 device = torch.device('cpu')
 print(f'Device: {device}')
@@ -29,15 +28,16 @@ LR = 0.0001
 BETA_01 = 0.92
 BETA_02 = 0.999
 # Max number of episodes
-MAX_EP = 3000
+MAX_EP = 4000
 # MAX_EP = 10
 # Frequency to update shared neural network
 UPDATE_GLOBAL_ITER = 5
 # UPDATE_GLOBAL_ITER = 10
 GAMMA = 0.9
+SEED = 0
 REWARDS = '../object/a3c_cartpole_reward.pkl'
 MODEL = '../model/a3c_cartpole.pt'
-
+SAVEFIG_01 = '../image/a3c_cartpole.png'
 print()
 
 
@@ -56,31 +56,28 @@ def np_to_tensor(np_array, dtype=np.float32):
 
 
 # Record result when episode ends
-def record(global_episode, global_episode_reward, local_episode_reward, res_queue, name):
+def record(shared_episode_counter, shared_episode_reward, local_episode_reward, shared_rewards_queue, name):
     """
     global_episode: mp.Value integer
     global_episode: mp.Value float
     """
     # Increment shared episode counter
-    with global_episode.get_lock():
-        global_episode.value += 1
+    with shared_episode_counter.get_lock():
+        shared_episode_counter.value += 1
 
     # Update shared total rewards per episode as weighted average
-    with global_episode_reward.get_lock():
-        if global_episode_reward.value == 0.0:
-            global_episode_reward.value = local_episode_reward
+    with shared_episode_reward.get_lock():
+        if shared_episode_reward.value == 0.0:
+            shared_episode_reward.value = local_episode_reward
         else:
-            global_episode_reward.value = (
-                global_episode_reward.value * 0.99 + local_episode_reward * 0.01
+            shared_episode_reward.value = (
+                shared_episode_reward.value * 0.99 + local_episode_reward * 0.01
             )
 
-    # Store current total rewards
-    res_queue.put(global_episode_reward.value)
-
     # Append local total reward to reward queue
-    res_queue.put(global_episode_reward.value)
+    shared_rewards_queue.put(shared_episode_reward.value)
 
-    print(f'{name} finished epeisode: {global_episode.value} with reward: {global_episode_reward.value}')
+    print(f'{name} finished epeisode: {shared_episode_counter.value} with reward: {shared_episode_reward.value:,.1f}')
 
 
 # Push local network to global network and then pull global network to local network
@@ -95,17 +92,17 @@ def push_and_pull(shared_optimizer,
                   gamma):
     # State value of next state
     if done:
-        v_s_ = 0.0
+        next_state_value = 0.0
     else:
         # network returns [logits, values], so [-1] returns state value
-        # Why [0, 0]?
-        v_s_ = local_network.forward(np_to_tensor(next_state[None, :]))[-1].data.numpy()[0, 0]
+        # forward(x).data.numpy() here returns data shape [1, 1], so to get a scalar [0, 0]
+        next_state_value = local_network.forward(np_to_tensor(next_state[None, :]))[-1].data.numpy()[0, 0]
 
     buffer_v_target = []
     # Calculate discounted total reward at each time step
     for r in buffer_reward[::-1]:
-        v_s_ = r + gamma * v_s_
-        buffer_v_target.append(v_s_)
+        next_state_value = r + gamma * next_state_value
+        buffer_v_target.append(next_state_value)
         # .reverse() updates the existing list
     buffer_v_target.reverse()
 
@@ -116,7 +113,7 @@ def push_and_pull(shared_optimizer,
         np_to_tensor(np.vstack(buffer_state)),
         # if else is just in case. It should be int64 in CartPole
         np_to_tensor(np.array(buffer_action), dtype=np.int64) if buffer_action[0].dtype == np.int64 else np_to_tensor(np.vstack(buffer_action)),
-        np_to_tensor(np.array(buffer_reward)[:, None])
+        np_to_tensor(np.array(buffer_v_target)[:, None])
     )
 
     # Push
@@ -159,11 +156,11 @@ class Network(nn.Module):
         # Action
         self.a_dim = a_dim
         # Policy
-        self.pi1 = nn.Linear(s_dim, 128)
-        self.pi2 = nn.Linear(128, a_dim)
+        self.pi1 = nn.Linear(s_dim, 256)
+        self.pi2 = nn.Linear(256, a_dim)
         # State value function
-        self.v1 = nn.Linear(s_dim, 128)
-        self.v2 = nn.Linear(128, 1)
+        self.v1 = nn.Linear(s_dim, 256)
+        self.v2 = nn.Linear(256, 1)
         # Initialize weights
         initialize_weights([self.pi1, self.pi2, self.v1, self.v2])
         # Creates a categorical distribution parameterized by probabilities here
@@ -174,45 +171,47 @@ class Network(nn.Module):
         x is state tensor.
         """
         # Logits for policy
-        pi1 = torch.tanh(self.pi1(x))
+        pi1 = torch.relu(self.pi1(x))
         logits = self.pi2(pi1)
         # State value function
-        v1 = torch.tanh(self.v1(x))
+        v1 = torch.relu(self.v1(x))
         values = self.v2(v1)
         return logits, values
 
-    def choose_action(self, s):
+    def choose_action(self, state):
         """
         s is state tensor
         """
         # Use eval() because it's inference
         self.eval()
-        logits, _ = self.forward(s)
+        logits, _ = self.forward(state)
         # logits has size ([n, action dimensions])
         prob = F.softmax(logits, dim=1).data
         m = self.distribution(probs=prob)
+        # m = self.distribution(prob)
         # numpy() returns shape (1,) so numpy()[0] returns a scalar
         action = m.sample().numpy()[0]
         return action
 
-    def loss_func(self, s, a, v_t):
+    def loss_func(self, state, action, target_values):
         """
         s is state buffer shape (n, state dimensions)
         a is action buffer shape (n,)
         v_t is target state value buffer shape (n, 1)
         """
         self.train()
-        logits, values = self.forward(s)
+        logits, values = self.forward(state)
 
         # Value loss, td is an estimate of advantage function
-        td = v_t - values
+        td = target_values - values
         c_loss = td.pow(2)
 
         # Policy loss
         probs = F.softmax(logits, dim=1)
         m = self.distribution(probs=probs)
+        # m = self.distribution(probs)
         # From A3C paper, why detach()?
-        exp_v = m.log_prob(a) * td.detach().squeeze()
+        exp_v = m.log_prob(action) * td.detach().squeeze()
         # Why negative?
         a_loss = -exp_v
         total_loss = (c_loss + a_loss).mean()
@@ -221,101 +220,104 @@ class Network(nn.Module):
 
 # Override the run method by subclassing mp.Process
 class Worker(mp.Process):
-    def __init__(self, global_network, global_optimizer, global_ep, global_ep_r, res_queue, name):
+    def __init__(self,
+                 shared_network,
+                 shared_optimizer,
+                 shared_episode_counter,
+                 shared_episode_reward,
+                 shared_rewards_queue,
+                 name):
         super(Worker, self).__init__()
+        # Shared elements
+        self.shared_network = shared_network
+        self.shared_optimizer = shared_optimizer
+        self.shared_episode_counter = shared_episode_counter
+        self.shared_episode_reward = shared_episode_reward
+        self.shared_rewards_queue = shared_rewards_queue
+        # Local elements
+        self.local_network = Network(N_S, N_A)
+        self.local_env = gym.make(ENV).unwrapped
+        self.local_env.seed(name)
         self.name = f'worker_{"0" + str(name) if name < 10 else str(name)}'
         print(f'{self.name} is made in process ID: {os.getpid()} and parent ID: {os.getppid()}')
-        # Number of episode shared
-        self.g_ep = global_ep
-        self.g_ep_r = global_ep_r
-        self.res_queue = res_queue
-        # Shared neural network
-        self.gnet = global_network
-        # Shared optimizer
-        self.opt = global_optimizer
-        # Local neural network
-        self.lnet = Network(N_S, N_A)
-        # Local environment of each worker
-        self.env = gym.make(ENV).unwrapped
 
     def run(self):
         print(f'*** {self.name} started in process ID: {os.getpid()} and parent ID: {os.getppid()} ***')
 
         # Total time step, not episode
-        total_step = 1
+        total_time_step = 1
 
         # The number of episodes experienced in training is shared over workers
         # When total number of episodes experience by all the workers exceeds a threshold,
         # A3C training ends
-        while self.g_ep.value < MAX_EP:
+        while self.shared_episode_counter.value < MAX_EP:
 
             # Initialize
-            s = self.env.reset()
+            state = self.local_env.reset()
             # States, actions, rewards
-            buffer_s, buffer_a, buffer_r = [], [], []
-            # Total rewards
-            ep_r = 0.0
+            buffer_state, buffer_action, buffer_reward = [], [], []
+            total_rewards = 0.0
 
             while True:
 
                 # Only print worker_00's interaction with environment
-                if self.name == 'worker_00':
-                    self.env.render()
+                if self.name == 'worker_01':
+                    self.local_env.render()
 
                 # Choose action
                 # s[None, :] change s.shape (4,) to s[None, :].shape (1, 4)
-                a = self.lnet.choose_action(np_to_tensor(s[None, :]))
+                action = self.local_network.choose_action(np_to_tensor(state[None, :]))
 
                 # Next state, reward, done
-                s_, r, done, _ = self.env.step(a)
+                next_state, reward, done, _ = self.local_env.step(action)
 
                 if done:
                     # Reward is collected even after termination, but we shouldn't
                     # CartPole gains 1 every time step, so subtract 1 from total reward
-                    r = -1
+                    reward = -1
 
-                ep_r += r
+                total_rewards += reward
 
-                buffer_a.append(a)
-                buffer_s.append(s)
-                buffer_r.append(r)
+                buffer_state.append(state)
+                buffer_action.append(action)
+                buffer_reward.append(reward)
 
                 # Update shared neural network only when episode ends or when at specified frequency
-                if total_step % UPDATE_GLOBAL_ITER or done:
+                if total_time_step % UPDATE_GLOBAL_ITER == 0 or done:
 
                     # Update shared neural network by local loss of gradients from local neural network
                     # Update local neural network parameters by the updated shared neural network parameters
                     push_and_pull(
-                        shared_optimizer=self.opt,
-                        local_network=self.lnet,
-                        shared_network=self.gnet,
+                        shared_optimizer=self.shared_optimizer,
+                        local_network=self.local_network,
+                        shared_network=self.shared_network,
                         done=done,
-                        next_state=s_,
-                        buffer_state=buffer_s,
-                        buffer_action=buffer_a,
-                        buffer_reward=buffer_r,
+                        next_state=next_state,
+                        buffer_state=buffer_state,
+                        buffer_action=buffer_action,
+                        buffer_reward=buffer_reward,
                         gamma=GAMMA
                     )
 
                     # Every time updating, clear experience
-                    buffer_s, buffer_a, buffer_r = [], [], []
+                    buffer_state, buffer_action, buffer_reward = [], [], []
 
                     if done:
                         record(
-                            global_episode=self.g_ep,
-                            global_episode_reward=self.g_ep_r,
-                            local_episode_reward=ep_r,
-                            res_queue=self.res_queue,
+                            shared_episode_counter=self.shared_episode_counter,
+                            shared_episode_reward=self.shared_episode_reward,
+                            local_episode_reward=total_rewards,
+                            shared_rewards_queue=self.shared_rewards_queue,
                             name=self.name
                         )
                         break
 
                 # Iterate to the next state
-                s = s_
-                total_step += 1
+                state = next_state
+                total_time_step += 1
 
         # Append None at the end of list of total rewards to call .join() to stop parallel programming
-        self.res_queue.put(None)
+        self.shared_rewards_queue.put(None)
 
 
 if __name__ == '__main__':
@@ -327,29 +329,39 @@ if __name__ == '__main__':
     print(f'Process ID: {os.getpid()}')
     print()
 
+    # Set seed
+    torch.manual_seed(SEED)
+    np.random.seed(SEED)
+    env.seed(SEED)
+
     # Shared network
-    global_network = Network(N_S, N_A)
+    shared_network = Network(N_S, N_A)
 
     # Share the shared network parameters in multiprocessing
-    global_network.share_memory()
+    shared_network.share_memory()
 
     # Shared optimizer
-    global_optimizer = SharedAdam(global_network.parameters(), lr=LR, betas=(BETA_01, BETA_02))
+    shared_optimizer = SharedAdam(shared_network.parameters(), lr=LR, betas=(BETA_01, BETA_02))
 
     # Initialize shared episode counter, shared ?, and shared ?
-    global_ep = mp.Value('i', 0)  # i indicates a signed integer, mp.Value to store values in a shared memory
-    global_ep_r = mp.Value('d', 0.)  # d indicates a double precision float, mp.Value to store values in a shared memory
-    res_queue = mp.Queue()  # Queue allows multiple processes to access the same queue object, meaning we don't need Lock
-    # print(f'global_ep: {global_ep}')
-    # print(f'global_ep_r: {global_ep_r}')
-    # print(f'res_queue: {res_queue}')
+    shared_episode_counter = mp.Value('i', 0)  # i indicates a signed integer, mp.Value to store values in a shared memory
+    shared_episode_reward = mp.Value('d', 0.)  # d indicates a double precision float, mp.Value to store values in a shared memory
+    shared_rewards_queue = mp.Queue()  # Queue allows multiple processes to access the same queue object, meaning we don't need Lock
 
     # Parallel training
-
     # Make parallel agents by mp.Process subclass Worker
     print('Make parallel agents')
-    workers = [Worker(global_network, global_optimizer, global_ep, global_ep_r, res_queue, i)
-               for i in range(NUM_WORKERS)]
+    workers = [
+        Worker(
+            shared_network,
+            shared_optimizer,
+            shared_episode_counter,
+            shared_episode_reward,
+            shared_rewards_queue,
+            i)
+        for i
+        in range(NUM_WORKERS)
+    ]
 
     # Start parallel training
     print()
@@ -358,7 +370,7 @@ if __name__ == '__main__':
 
     total_rewards = []
     while True:
-        reward = res_queue.get()
+        reward = shared_rewards_queue.get()
         if reward is not None:
             total_rewards.append(reward)
         else:
@@ -373,10 +385,13 @@ if __name__ == '__main__':
     pickle.dump(total_rewards, open(REWARDS, 'wb'))
 
     # Save model
-    torch.save(global_network.state_dict(), MODEL)
+    torch.save(shared_network.state_dict(), MODEL)
 
     # Show rewards
     plt.plot(total_rewards)
-    plt.xlabel('Step')
-    plt.ylabel('Moving average ep reward')
+    plt.title(f'Asynchronous Advantage Actor Critic CartPole')
+    plt.xlabel('Episode')
+    plt.ylabel('Moving average of episode rewards')
+    plt.tight_layout()
+    plt.savefig(SAVEFIG_01)
     plt.show()
