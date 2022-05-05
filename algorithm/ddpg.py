1291761bf29f6e915a7028048f16550a11f5c96e
Yuki Kitayama <ykitayama@corp.guzman.com>
2022-05-05 11:15:08 -0600

    forgot some stuff to commit

---
+++ algorithm/ddpg.py
@@ -0,0 +1,21 @@
+# import gym
+import torch
+import torch.nn as nn
+
+
+# Parameter
+ENV = 'Pendulum-v0'
+
+# class QValueNetwork(nn.Module):
+
+
+
+def main():
+
+    # Environment
+    # env = gym.make(ENV)
+    print('test')
+
+
+if __name__ == '__main__':
+    main()
